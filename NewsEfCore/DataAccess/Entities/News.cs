694a9d0b0659de1b74ea9e5289997df7b9263b18
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-02-22 21:56:20 -0800

    Added news entity.

---
+++ NewsEfCore/DataAccess/Entities/News.cs
@@ -0,0 +1,19 @@
+﻿namespace NewsEfCore.DataAccess.Entities
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int WriterId { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public string ImageName { get; set; }
+        public int CategoryId { get; set; }
+
+        #region navigation Propety
+
+        
+
+        #endregion
+    }
+}
