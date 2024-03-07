c981d8a5d8b410354b1cb02f7c10984589928690
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-03-07 00:59:44 -0800

    Added NewsController and GetByCategory action.

---
+++ NewsEfCore/Controllers/NewsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsEfCore.Services.Interfaces;
+
+namespace NewsEfCore.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly INewsService _newsService;
+        private readonly ICategoryService _categoryService;
+
+        public NewsController(INewsService newsService, ICategoryService categoryService)
+        {
+            _newsService = newsService;
+            _categoryService = categoryService;
+        }
+
+        public IActionResult GetByCategory(int id)
+        {
+            ViewBag.Category = _categoryService.GetById(id).Title;
+            return View(_newsService.GetByCategory(id));
+        }
+       
+    }
+}
